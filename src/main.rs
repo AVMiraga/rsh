78e010f9809639b8907a5f4a69d05a92e6e3ca4e
AVMiraga <mirishsimov@gmail.com>
2025-12-24 17:09:14 +0400

    impl exit

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+const VALID_COMMANDS: &[&str] = &["exit"];
 
+fn main() {
     loop {
         let mut command = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).unwrap();
-        println!("{}: command not found", &command.trim());
+
+        if !VALID_COMMANDS.contains(&command.as_str().trim()) {
+            println!("{}: command not found", &command.trim());
+        }
+
+        if command.trim() == "exit" {
+            break;
+        }
     }
 }

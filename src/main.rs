5e837316c5e45d2efd418dcbcabda8bf9776839e
AVMiraga <mirishsimov@gmail.com>
2025-12-27 23:50:10 +0400

    impl appending (stderr and stdout)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use pathsearch::find_executable_in_path;
 use shlex::split;
 use std::env;
 use std::env::{current_dir, set_current_dir};
+use std::fs::OpenOptions;
 use std::io::{self, Write, stderr, stdout};
 use std::path::Path;
 use std::process::Command;
@@ -52,6 +53,20 @@ fn testing() {
 }
 
 fn main() {
+    enum RedirectionKind {
+        Stdout,
+        Stderr,
+        AppendStdout,
+        AppendStderr,
+    }
+
+    let redirections = [
+        (vec![">", "1>"], RedirectionKind::Stdout),
+        (vec!["2>"], RedirectionKind::Stderr),
+        (vec![">>", "1>>"], RedirectionKind::AppendStdout),
+        (vec!["2>>"], RedirectionKind::AppendStderr),
+    ];
+
     loop {
         let mut command = String::new();
         print!("$ ");
@@ -62,47 +77,72 @@ fn main() {
         let command = whole_command.first().unwrap();
         let arguments = whole_command[1..].to_vec();
 
-        let mut argument_iter = arguments.iter();
-        let mut is_redirection = false;
-        let mut is_err_redirection = false;
+        let mut redir_kind = &RedirectionKind::Stdout;
         let mut to_file = "";
         let mut from_content: Vec<String> = vec![];
 
-        match argument_iter.position(|s| s == ">" || s == "1>") {
-            Some(redirect_pos) => {
-                is_redirection = true;
+        for (ops, kind) in &redirections {
+            let mut argument_iter = arguments.iter();
+            if let Some(redirect_pos) = argument_iter.position(|s| ops.contains(&s.as_str())) {
                 if let Some(file) = argument_iter.next() {
+                    redir_kind = kind;
                     to_file = file;
+                    from_content = arguments[..redirect_pos].to_vec();
                 }
-                from_content = arguments[..redirect_pos].to_vec();
             }
-            None => {}
         }
 
-        let mut argument_iter = arguments.iter();
+        // match argument_iter.position(|s| s == ">" || s == "1>") {
+        //     Some(redirect_pos) => {
+        //         is_redirection = true;
+        //         if let Some(file) = argument_iter.next() {
+        //             to_file = file;
+        //         }
+        //         from_content = arguments[..redirect_pos].to_vec();
+        //     }
+        //     None => {}
+        // }
 
-        match argument_iter.position(|s| s == "2>") {
-            Some(redirect_pos) => {
-                is_err_redirection = true;
-                is_redirection = false;
-                if let Some(file) = argument_iter.next() {
-                    to_file = file;
-                }
-                from_content = arguments[..redirect_pos].to_vec();
-            }
-            None => {}
-        }
+        // let mut file = OpenOptions::new().append().open(to_file).unwrap();
+
+        // if is_redirection || is_err_redirection {}
 
         match command.trim() {
             "exit" => break,
             "echo" => {
-                if is_redirection {
-                    std::fs::write(to_file, from_content.join(" ")).unwrap();
-                } else if is_err_redirection {
-                    println!("{}", from_content.join(" ").trim());
-                    std::fs::write(to_file, "").unwrap();
-                } else {
+                if to_file.is_empty() {
                     println!("{}", arguments.join(" ").trim());
+                } else {
+                    let is_append = matches!(
+                        redir_kind,
+                        RedirectionKind::AppendStdout | RedirectionKind::AppendStderr
+                    );
+                    let mut file = OpenOptions::new()
+                        .create(true)
+                        .append(is_append)
+                        .truncate(!is_append)
+                        .write(true)
+                        .open(&to_file)
+                        .unwrap();
+
+                    let mut from_content = from_content.join(" ");
+                    from_content.push_str("\n");
+
+                    match redir_kind {
+                        RedirectionKind::Stdout | RedirectionKind::AppendStdout => {
+                            file.write_all(from_content.as_bytes()).unwrap();
+                        }
+                        RedirectionKind::Stderr | RedirectionKind::AppendStderr => {
+                            file.write_all("".as_bytes()).unwrap();
+                            println!("{}", from_content.trim());
+                        }
+                    }
+                    // if is_redirection {
+                    //     std::fs::write(to_file, from_content.join(" ")).unwrap();
+                    // } else if is_err_redirection {
+                    //     println!("{}", from_content.join(" ").trim());
+                    //     std::fs::write(to_file, "").unwrap();
+                    // }
                 }
             }
             "type" => {
@@ -141,25 +181,43 @@ fn main() {
             _ => match find_executable_in_path(command.trim()) {
                 Some(_) => {
                     let out = Command::new(command)
-                        .args(if is_redirection || is_err_redirection {
+                        .args(if !to_file.is_empty() {
                             &from_content
                         } else {
                             &arguments
                         })
                         .output()
                         .unwrap();
-                    if is_redirection {
-                        std::fs::write(to_file, &out.stdout).unwrap();
-                        stderr().write_all(&out.stderr).unwrap();
-                    } else if is_err_redirection {
-                        std::fs::write(to_file, &out.stderr).unwrap();
+
+                    if to_file.is_empty() || !out.status.success() {
                         stdout().write_all(&out.stdout).unwrap();
-                    } else {
-                        stdout().write_all(&out.stdout).unwrap();
-                        if out.stdout.last() != Some(&b'\n') {
+                        if out.status.success() && out.stdout.last() != Some(&b'\n') {
                             println!();
                         }
                         stderr().write_all(&out.stderr).unwrap();
+                    } else {
+                        let is_append = matches!(
+                            redir_kind,
+                            RedirectionKind::AppendStdout | RedirectionKind::AppendStderr
+                        );
+                        let mut file = OpenOptions::new()
+                            .create(true)
+                            .append(is_append)
+                            .truncate(!is_append)
+                            .write(true)
+                            .open(&to_file)
+                            .unwrap();
+
+                        match redir_kind {
+                            RedirectionKind::Stdout | RedirectionKind::AppendStdout => {
+                                file.write_all(&out.stdout).unwrap();
+                                stderr().write_all(&out.stderr).unwrap();
+                            }
+                            RedirectionKind::Stderr | RedirectionKind::AppendStderr => {
+                                file.write_all(&out.stderr).unwrap();
+                                stdout().write(&out.stdout).unwrap();
+                            }
+                        }
                     }
                 }
                 _ => println!("{}: command not found", &command.trim()),

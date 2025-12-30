9b9d465f68d110de87d9a07ee0733c1718f3de20
AVMiraga <mirishsimov@gmail.com>
2025-12-30 12:08:58 +0400

    impl tab

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,21 @@
+use crossterm::event::{Event, KeyCode, KeyEventKind, KeyModifiers, ModifierKeyCode, read};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use pathsearch::find_executable_in_path;
 use shlex::split;
-use std::env;
-use std::env::{current_dir, set_current_dir};
-use std::fs::OpenOptions;
-use std::io::{self, Write, stderr, stdout};
-use std::path::Path;
-use std::process::Command;
-
-// #[cfg(windows)]
-// const PATH_SEP: char = ';';
-// #[cfg(not(windows))]
-// const PATH_SEP: char = ':';
+use std::{
+    env::{self, current_dir, set_current_dir},
+    fs::OpenOptions,
+    io::{Write, stderr, stdout},
+    path::Path,
+    process::Command,
+};
 
 const VALID_COMMANDS_BUILTIN: &[&str] = &["echo", "exit", "type", "pwd", "cd", ".", ".."];
 
 #[test]
-fn testing() {
-    let command = String::from("echo hello hello > output.txt");
+fn testing() {}
 
-    let whole_command = split(command.trim()).unwrap_or([].to_vec());
-    let command = whole_command.first().unwrap();
-    let arguments = whole_command[1..].to_vec();
-
-    let mut argument_iter = arguments.iter();
-    let mut is_redirection = false;
-    let mut to_file = "";
-    let mut from_content: Vec<String> = vec![];
-
-    match argument_iter.position(|s| s == ">" || s == "1>") {
-        Some(redirect_pos) => {
-            is_redirection = true;
-            if let Some(file) = argument_iter.next() {
-                to_file = file;
-            }
-            from_content = arguments[..redirect_pos].to_vec();
-        }
-        None => {}
-    }
-
-    dbg!(&to_file);
-    dbg!(&from_content);
-
-    // if let Some(redirect_operator_pos) = argument_iter.position(|s| s == ">" || s == "1>") {
-    //     if let Some(filename) = argument_iter.next() {
-    //         dbg!(filename);
-    //     }
-    // }
-
-    dbg!(&arguments);
-    dbg!(&command);
-    dbg!(whole_command);
-}
-
-fn main() {
+fn main() -> std::io::Result<()> {
     enum RedirectionKind {
         Stdout,
         Stderr,
@@ -69,166 +32,206 @@ fn main() {
 
     loop {
         let mut command = String::new();
-        print!("$ ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut command).unwrap();
 
-        let whole_command = split(command.trim()).unwrap_or([].to_vec());
-        let command = whole_command.first().unwrap();
-        let arguments = whole_command[1..].to_vec();
+        // stdin().read_line(&mut command).unwrap();
+        // stdout().flush().unwrap();
+        print!("\r$ ");
+        stdout().flush()?;
 
-        let mut redir_kind = &RedirectionKind::Stdout;
-        let mut to_file = "";
-        let mut from_content: Vec<String> = vec![];
-
-        for (ops, kind) in &redirections {
-            let mut argument_iter = arguments.iter();
-            if let Some(redirect_pos) = argument_iter.position(|s| ops.contains(&s.as_str())) {
-                if let Some(file) = argument_iter.next() {
-                    redir_kind = kind;
-                    to_file = file;
-                    from_content = arguments[..redirect_pos].to_vec();
+        loop {
+            enable_raw_mode()?;
+            if let Event::Key(k) = read()? {
+                if k.kind != KeyEventKind::Press {
+                    continue;
                 }
-            }
-        }
 
-        // match argument_iter.position(|s| s == ">" || s == "1>") {
-        //     Some(redirect_pos) => {
-        //         is_redirection = true;
-        //         if let Some(file) = argument_iter.next() {
-        //             to_file = file;
-        //         }
-        //         from_content = arguments[..redirect_pos].to_vec();
-        //     }
-        //     None => {}
-        // }
+                if k.modifiers.contains(KeyModifiers::CONTROL) && k.code.is_char('c') {
+                    return Ok(());
+                }
 
-        // let mut file = OpenOptions::new().append().open(to_file).unwrap();
-
-        // if is_redirection || is_err_redirection {}
-
-        match command.trim() {
-            "exit" => break,
-            "echo" => {
-                if to_file.is_empty() {
-                    println!("{}", arguments.join(" ").trim());
-                } else {
-                    let is_append = matches!(
-                        redir_kind,
-                        RedirectionKind::AppendStdout | RedirectionKind::AppendStderr
-                    );
-                    let mut file = OpenOptions::new()
-                        .create(true)
-                        .append(is_append)
-                        .truncate(!is_append)
-                        .write(true)
-                        .open(&to_file)
-                        .unwrap();
-
-                    let from_content = from_content.join(" ");
-
-                    // if matches!(
-                    //     redir_kind,
-                    //     RedirectionKind::AppendStdout | RedirectionKind::AppendStderr
-                    // ) {
-                    //     from_content.push_str("\n");
-                    // }
-
-                    match redir_kind {
-                        RedirectionKind::Stdout | RedirectionKind::AppendStdout => {
-                            file.write_all(from_content.as_bytes()).unwrap();
-                            file.write_all("\n".as_bytes()).unwrap();
+                match k.code {
+                    KeyCode::Tab => {
+                        let possible_cmd: Vec<String> = VALID_COMMANDS_BUILTIN
+                            .iter()
+                            .filter(|x| x.starts_with(command.as_str()))
+                            .map(|x| x.to_string())
+                            .collect();
+                        if possible_cmd.is_empty() {
+                            print!("\x07");
+                            continue;
                         }
-                        RedirectionKind::Stderr | RedirectionKind::AppendStderr => {
-                            file.write_all("".as_bytes()).unwrap();
-                            println!("{}", from_content.trim());
-                        }
+                        command = possible_cmd[0].to_string();
+                        print!("\r\x1b[2K$ {}", command);
+
+                        stdout().flush()?;
                     }
-                    // if is_redirection {
-                    //     std::fs::write(to_file, from_content.join(" ")).unwrap();
-                    // } else if is_err_redirection {
-                    //     println!("{}", from_content.join(" ").trim());
-                    //     std::fs::write(to_file, "").unwrap();
-                    // }
-                }
-            }
-            "type" => {
-                if VALID_COMMANDS_BUILTIN.contains(&arguments.join(" ").trim()) {
-                    println!("{} is a shell builtin", arguments.join(" ").trim());
-                } else if let Some(path) = find_executable_in_path(&arguments.join(" ").trim()) {
-                    println!(
-                        "{} is {}",
-                        &arguments.join(" ").trim(),
-                        path.to_str().unwrap()
-                    );
-                } else {
-                    println!("{}: not found", arguments.join(" ").trim());
-                }
-            }
-            "pwd" => {
-                println!("{}", current_dir().unwrap().to_str().unwrap());
-            }
-            "." => {
-                set_current_dir(current_dir().unwrap()).unwrap();
-            }
-            ".." => {
-                let new_dir = current_dir().unwrap().pop().to_string();
-                set_current_dir(new_dir).unwrap();
-            }
-            "cd" => {
-                let new_arg =
-                    &arguments[0].replace("~", env::home_dir().unwrap().to_str().unwrap());
-                let new_dir = Path::new(new_arg).to_path_buf();
-
-                match set_current_dir(new_dir) {
-                    Ok(_) => {}
-                    Err(_) => println!("cd: {}: No such file or directory", new_arg),
-                }
-            }
-            _ => match find_executable_in_path(command.trim()) {
-                Some(_) => {
-                    let out = Command::new(command)
-                        .args(if !to_file.is_empty() {
-                            &from_content
-                        } else {
-                            &arguments
-                        })
-                        .output()
-                        .unwrap();
-
-                    if to_file.is_empty() {
-                        stdout().write_all(&out.stdout).unwrap();
-                        if out.status.success() && out.stdout.last() != Some(&b'\n') {
+                    KeyCode::Char(c) => {
+                        command.push(c);
+                        print!("{c}");
+                        stdout().flush()?;
+                    }
+                    KeyCode::Enter => {
+                        disable_raw_mode()?;
+                        if command.len() == 0 {
                             println!();
+                            print!("\r$ ");
+                            stdout().flush()?;
+                            continue;
                         }
-                        stderr().write_all(&out.stderr).unwrap();
-                    } else {
-                        let is_append = matches!(
-                            redir_kind,
-                            RedirectionKind::AppendStdout | RedirectionKind::AppendStderr
-                        );
-                        let mut file = OpenOptions::new()
-                            .create(true)
-                            .append(is_append)
-                            .truncate(!is_append)
-                            .write(true)
-                            .open(&to_file)
-                            .unwrap();
+                        println!();
+                        stdout().flush()?;
+                        let whole_command = split(command.trim()).unwrap_or([].to_vec());
+                        command.clear();
+                        let command = whole_command.first().unwrap();
+                        let arguments = whole_command[1..].to_vec();
 
-                        match redir_kind {
-                            RedirectionKind::Stdout | RedirectionKind::AppendStdout => {
-                                file.write_all(&out.stdout).unwrap();
-                                stderr().write_all(&out.stderr).unwrap();
+                        let mut redir_kind = &RedirectionKind::Stdout;
+                        let mut to_file = "";
+                        let mut from_content: Vec<String> = vec![];
+
+                        for (ops, kind) in &redirections {
+                            let mut argument_iter = arguments.iter();
+                            if let Some(redirect_pos) =
+                                argument_iter.position(|s| ops.contains(&s.as_str()))
+                            {
+                                if let Some(file) = argument_iter.next() {
+                                    redir_kind = kind;
+                                    to_file = file;
+                                    from_content = arguments[..redirect_pos].to_vec();
+                                }
                             }
-                            RedirectionKind::Stderr | RedirectionKind::AppendStderr => {
-                                file.write_all(&out.stderr).unwrap();
-                                stdout().write(&out.stdout).unwrap();
+                        }
+                        match command.trim() {
+                            "exit" => break,
+                            "echo" => {
+                                if to_file.is_empty() {
+                                    println!("{}", arguments.join(" ").trim());
+                                } else {
+                                    let is_append = matches!(
+                                        redir_kind,
+                                        RedirectionKind::AppendStdout
+                                            | RedirectionKind::AppendStderr
+                                    );
+                                    let mut file = OpenOptions::new()
+                                        .create(true)
+                                        .append(is_append)
+                                        .truncate(!is_append)
+                                        .write(true)
+                                        .open(&to_file)?;
+
+                                    let from_content = from_content.join(" ");
+
+                                    match redir_kind {
+                                        RedirectionKind::Stdout | RedirectionKind::AppendStdout => {
+                                            file.write_all(from_content.as_bytes())?;
+                                            file.write_all("\n".as_bytes())?;
+                                        }
+                                        RedirectionKind::Stderr | RedirectionKind::AppendStderr => {
+                                            file.write_all("".as_bytes())?;
+                                            println!("{}", from_content.trim());
+                                        }
+                                    }
+                                }
                             }
+                            "type" => {
+                                if VALID_COMMANDS_BUILTIN.contains(&arguments.join(" ").trim()) {
+                                    println!("{} is a shell builtin", arguments.join(" ").trim());
+                                } else if let Some(path) =
+                                    find_executable_in_path(&arguments.join(" ").trim())
+                                {
+                                    println!(
+                                        "{} is {}",
+                                        &arguments.join(" ").trim(),
+                                        path.to_str().unwrap()
+                                    );
+                                } else {
+                                    println!("{}: not found", arguments.join(" ").trim());
+                                }
+                            }
+                            "pwd" => {
+                                println!("{}", current_dir()?.to_str().unwrap());
+                            }
+                            "." => {
+                                set_current_dir(current_dir()?)?;
+                            }
+                            ".." => {
+                                let new_dir = current_dir()?.pop().to_string();
+                                set_current_dir(new_dir)?;
+                            }
+                            "cd" => {
+                                let new_arg = &arguments[0]
+                                    .replace("~", env::home_dir().unwrap().to_str().unwrap());
+                                let new_dir = Path::new(new_arg).to_path_buf();
+
+                                match set_current_dir(new_dir) {
+                                    Ok(_) => {}
+                                    Err(_) => {
+                                        println!("cd: {}: No such file or directory", new_arg)
+                                    }
+                                }
+                            }
+                            _ => match find_executable_in_path(command.trim()) {
+                                Some(_) => {
+                                    let out = Command::new(command)
+                                        .args(if !to_file.is_empty() {
+                                            &from_content
+                                        } else {
+                                            &arguments
+                                        })
+                                        .output()?;
+
+                                    if to_file.is_empty() {
+                                        stdout().write_all(&out.stdout)?;
+                                        if out.status.success() && out.stdout.last() != Some(&b'\n')
+                                        {
+                                            println!();
+                                        }
+                                        stderr().write_all(&out.stderr)?;
+                                    } else {
+                                        let is_append = matches!(
+                                            redir_kind,
+                                            RedirectionKind::AppendStdout
+                                                | RedirectionKind::AppendStderr
+                                        );
+                                        let mut file = OpenOptions::new()
+                                            .create(true)
+                                            .append(is_append)
+                                            .truncate(!is_append)
+                                            .write(true)
+                                            .open(&to_file)?;
+
+                                        match redir_kind {
+                                            RedirectionKind::Stdout
+                                            | RedirectionKind::AppendStdout => {
+                                                file.write_all(&out.stdout)?;
+                                                stderr().write_all(&out.stderr)?;
+                                            }
+                                            RedirectionKind::Stderr
+                                            | RedirectionKind::AppendStderr => {
+                                                file.write_all(&out.stderr)?;
+                                                stdout().write(&out.stdout)?;
+                                            }
+                                        }
+                                    }
+                                }
+                                _ => println!("{}: command not found", &command.trim()),
+                            },
+                        }
+                        print!("\r$ ");
+                        stdout().flush()?;
+                    }
+                    KeyCode::Backspace => {
+                        if command.len() > 0 {
+                            command.pop();
+                            print!("\x08 \x08");
+                            stdout().flush()?;
                         }
                     }
+                    _ => {}
                 }
-                _ => println!("{}: command not found", &command.trim()),
-            },
+            }
         }
     }
+
+    Ok(())
 }
